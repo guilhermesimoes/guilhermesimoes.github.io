351e2f9836932b8274598396101530c0a5c5038d
Guilherme Simoes <guilherme.rdems@gmail.com>
2018-01-05 16:44:06 +0000

    Add anchor links to headers and paragraphs with AnchorJS
    https://www.bryanbraun.com/anchorjs/

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,1 +1,15 @@
+//= require anchor-js/anchor.min.js
 //= require_tree .
+
+document.addEventListener('DOMContentLoaded', function() {
+  anchors.options = {
+    placement: 'left'
+  };
+  anchors.add(); // headers
+
+  anchors.options = {
+    placement: 'left',
+    icon: '¶'
+  };
+  anchors.add('.post-content > p');
+});
