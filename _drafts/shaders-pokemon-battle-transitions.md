55eb3746533d2aa692211953f7b1cdbad21f76cc
Guilherme Simoes <guilherme.rdems@gmail.com>
2020-10-04 04:17:08 +0100

    asd

--- _drafts/shaders-pokemon-battle-transitions.md
+++ _drafts/shaders-pokemon-battle-transitions.md
@@ -2,11 +2,26 @@
 layout:   post
 title:    "Shaders"
 ---
+<div class="shaders">
+  <svg class="svg-symbol-defs">
+    <defs>
+      <path id="pause-icon" data-next-state="play-icon" d="M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26" />
+      <path id="play-icon" data-next-state="pause-icon" d="M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28" />
+    </defs>
+  </svg>
 
-<div class="aspect-ratio-box ratio-game-boy">
-  <canvas id="canvas" class="aspect-ratio-box-content" />
+  <div class="aspect-ratio-box ratio-game-boy">
+    <canvas id="canvas" class="aspect-ratio-box-content" />
+  </div>
+  <div class="slider-container">
+    <button class="play-pause-button js-play-pause-button">
+      <svg viewBox="0 0 36 36">
+        <use xlink:href="#play-icon" />
+      </svg>
+    </button>
+    <input type="range" min="0" max="100" value="0" class="slider">
+  </div>
 </div>
-<input type="range" min="0" max="100" value="0" class="shader-slider">
 
 <script type="text/javascript" src="/assets/js/regl-2.0.1.min.js"></script>
 
--- assets/js/shaders-pokemon-battle-transitions.js
+++ assets/js/shaders-pokemon-battle-transitions.js
@@ -6,15 +6,28 @@ window.addEventListener('load', async function onLoad() {
   }
 
   var imagePromises = loadImages(regl);
-
+  var playPauseButton = new PlayPauseButton(document.querySelector(".js-play-pause-button"));
+  var slider = document.querySelector('.slider');
   var regl = createREGL({ gl: gl });
+
+  var numPoints = 160 * 144;
+  var points = [];
+  for (var i=0; i<160; i++) {
+    for (var j=0; j<144; j++) {
+      points.push([i / 160, j / 144]);
+    }
+  }
 
   var drawScreen = regl({
     frag: `
     precision mediump float;
     uniform sampler2D texture;
+    uniform float cutoff;
     varying vec2 uv;
     void main () {
+      if (uv.x < cutoff) {
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+      }
       gl_FragColor = texture2D(texture, uv);
     }`,
 
@@ -24,58 +37,34 @@ window.addEventListener('load', async function onLoad() {
     varying vec2 uv;
     void main () {
       uv = position;
-      gl_Position = vec4(1.0 - 2.0 * position, 0, 1);
+      gl_Position = vec4(position * 2.0 - 1.0, 0, 1);
     }`,
 
     attributes: {
-      position: [
-        -2, 0,
-        0, -2,
-        2, 2]
+      position: points
     },
+
+    primitive: 'points',
+
+    count: numPoints,
 
     uniforms: {
-      texture: (context, props, batchId) => props.texture,
+      texture: regl.prop('texture'),
+      cutoff: regl.prop('cutoff')
     },
-
-    count: 3
   });
 
   var images = await imagePromises;
   textures = images.map(regl.texture);
   console.log(textures);
 
-  // const drawTriangle = regl({
-  //   frag: `
-  //   void main() {
-  //     gl_FragColor = vec4(1, 0, 0, 1);
-  //   }`,
-
-  //   vert: `
-  //   attribute vec2 position;
-  //   void main() {
-  //     gl_Position = vec4(position, 0, 1);
-  //   }`,
-
-  //   attributes: {
-  //     position: [[0, -1], [-1, 0], [1, 1]]
-  //   },
-
-  //   count: 3
-  // });
-
-
-
-
-  // regl.frame(function () {
-  //   regl.clear({
-  //     color: [0, 0, 0, 1]
-  //   })
-  // });
-
-  drawScreen({ texture: textures[0] });
-
-
+  var cutoff;
+  regl.frame(function () {
+    if (slider.value !== cutoff) {
+      cutoff = slider.value;
+      drawScreen({ texture: textures[0], cutoff: parseFloat(cutoff) });
+    }
+  });
 });
 
 function onload2promise(obj){
@@ -104,3 +93,20 @@ async function loadImages() {
 //   requestAnimationFrame(render);
 // }
 // requestAnimationFrame(render);
+
+class PlayPauseButton {
+    constructor(el) {
+      this.el = el;
+      this.animationDuration = 350;
+      this.el.addEventListener('click', this.goToNextState.bind(this));
+    }
+
+    goToNextState() {
+      var useEl = this.el.querySelector('use');
+      var iconId = useEl.getAttribute('xlink:href');
+      var currentIcon = document.querySelector(iconId);
+      var nextIconId = '#' + currentIcon.getAttribute('data-next-state');
+
+      useEl.setAttribute('xlink:href', nextIconId);
+    }
+};
